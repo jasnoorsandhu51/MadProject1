e6e3c30424c63dea63918a6512a7052bb819bad1
Farhiya <fafrah003@gmail.com>
2026-03-19 15:22:49 -0400

    Added timer to the level screen

--- mystery_puzzle_game/lib/screens/level_screen.dart
+++ mystery_puzzle_game/lib/screens/level_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LevelScreen extends StatelessWidget {
+class LevelScreen extends StatefulWidget {
   final int level;
   final String targetObject;
 
@@ -11,10 +11,18 @@ class LevelScreen extends StatelessWidget {
   });
 
   @override
+  State<LevelScreen> createState() => _LevelScreenState();
+}
+
+//Timer that counts down from 60 seconds
+class _LevelScreenState extends State<LevelScreen> {
+  int timeLeft = 60;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Level $level")),
-      body: const Center(child: Text("Level Screen")),
+      appBar: AppBar(title: Text("Level ${widget.level}")),
+      body: Center(child: Text("Time Left: $timeLeft")),
     );
   }
 }
