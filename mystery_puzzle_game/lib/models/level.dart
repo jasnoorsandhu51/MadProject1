9086a1fe1b466d4cee7e6d8d61ac44be76598dc2
Farhiya <fafrah003@gmail.com>
2026-03-22 12:11:22 -0400

    Updated the level.dart

---
+++ mystery_puzzle_game/lib/models/level.dart
@@ -0,0 +1,7 @@
+// Stores the level data and correct object
+class Level {
+  final int levelNumber;
+  final String targetObject;
+
+  Level({required this.levelNumber, required this.targetObject});
+}
